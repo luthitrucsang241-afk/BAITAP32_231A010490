31bfc7ae9c2df1ea1ca83c62ca84678f0e3878c9
luthitrucsang241-afk <luthitrucsang241@gmail.com>
2026-07-17 07:46:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Portfolio của Lữ Thị Trúc Sang đã load xong!");
+
+    alert("Chào mừng bạn đến với portfolio thật trên Internet của mình!");
+});
